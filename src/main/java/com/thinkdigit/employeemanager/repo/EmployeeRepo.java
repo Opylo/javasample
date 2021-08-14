4bbec6a7f615a9f73450b53de045f7c00da35dcf
Opylo <optimartnetcom121@gmail.com>
2021-08-14 20:29:28 +0100

    added a info

---
+++ src/main/java/com/thinkdigit/employeemanager/repo/EmployeeRepo.java
@@ -0,0 +1,13 @@
+package com.thinkdigit.employeemanager.repo;
+
+import com.thinkdigit.employeemanager.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+    void deleteEmployeeById(Long id);
+    //finding each employee by id
+    Optional<Employee> findEmployeeById(Long id);
+    //this close the class
+}
